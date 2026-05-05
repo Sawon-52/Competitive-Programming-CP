a60a5ca7be2edbec84754dd722baa99701419131
Sawon-52 <mehedihasansawon.52@gmail.com>
2026-05-06 00:19:55 +0600

    cp

---
+++ power.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int result = 1;
+    for(int i = 0; i < b; i++) {
+        result *= a;
+    }
+
+    cout << result << endl;
+    return 0;
+}
