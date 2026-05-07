67d0f8e50ca3537262826dd83e3b8af5b41ede89
Sawon-52 <mehedihasansawon.52@gmail.com>
2026-05-07 17:52:45 +0600

    cp

---
+++ food_cost.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    int sixDayCost = 6 * a;
+
+    cout << sixDayCost+b << endl;
+    return 0;
+}
+    
---
+++ notes.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cin>>n;
+    cout<<(n*2000)/500<<endl;
+    return 0;
+}
